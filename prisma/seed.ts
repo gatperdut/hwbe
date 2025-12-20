09dc8208c3f29ec1705ca3c7f84d41b684d7e987
Carlos Romero Breto <carlos.romero.breto@gmail.com>
2025-12-20 17:17:13 +0100

    user seeds

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
+import { PrismaClient } from 'src/generated/client';
+import { seedUsers } from './seeds/user.seed';
+
+const adapter: PrismaPg = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  await seedUsers(prisma);
+}
+main()
+  .then(async (): Promise<void> => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+
+    await prisma.$disconnect();
+
+    process.exit(1);
+  });
---
+++ prisma/seeds/user.seed.ts
@@ -0,0 +1,68 @@
+import * as bcrypt from 'bcrypt';
+import { PrismaClient } from 'src/generated/client';
+
+export async function seedUsers(prisma: PrismaClient) {
+  const salt: string = await bcrypt.genSalt();
+
+  await prisma.user.upsert({
+    where: { email: 'crb@gmail.com' },
+    update: {},
+    create: {
+      email: 'crb@gmail.com',
+      displayName: 'Carlos',
+      password: await bcrypt.hash('crbcrb', salt),
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { email: 'jam@gmail.com' },
+    update: {},
+    create: {
+      email: 'jam@gmail.com',
+      displayName: 'Josep',
+      password: await bcrypt.hash('jamjam', salt),
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { email: 'jps@gmail.com' },
+    update: {},
+    create: {
+      email: 'jps@gmail.com',
+      displayName: 'Javi',
+      password: await bcrypt.hash('jpsjps', salt),
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { email: 'vfg@gmail.com' },
+    update: {},
+    create: {
+      email: 'vfg@gmail.com',
+      displayName: 'Vicent',
+      password: await bcrypt.hash('vfgvfg', salt),
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { email: 'vps@gmail.com' },
+    update: {},
+    create: {
+      email: 'vps@gmail.com',
+      displayName: 'Victor',
+      password: await bcrypt.hash('vpsvps', salt),
+    },
+  });
+
+  for (let i = 0; i < 10; i++) {
+    await prisma.user.upsert({
+      where: { email: `email${i}@gmail.com` },
+      update: {},
+      create: {
+        email: `email${i}@gmail.com`,
+        displayName: `User ${i}`,
+        password: await bcrypt.hash(`password${i}`, salt),
+      },
+    });
+  }
+}
